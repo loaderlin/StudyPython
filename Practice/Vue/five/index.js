67a0f46685af5acd6bbd8dbd2d4a4b6bf68afac0
loaderlin <loaderlin@163.com>
2018-10-05 16:10:32 +0800

    练习一： 全选，勾选商品结算

--- Practice/Vue/five/index.js
+++ Practice/Vue/five/index.js
@@ -3,17 +3,20 @@ var app = new Vue({
     data: {
         list: [
             { id: 1, name: 'iPhone 7', price: 6188, count: 1},
-            { id: 1, name: 'iPhone Pro', price: 5888, count: 1},
-            { id: 1, name: 'MacBook pro', price: 21488, count: 1},
-            { id: 1, name: 'iPhone XS ', price: 10999, count: 1},
-            { id: 1, name: 'iPhone XS Max', price: 20999, count: 1}
-        ]
+            { id: 2, name: 'iPhone Pro', price: 5888, count: 1},
+            { id: 3, name: 'MacBook pro', price: 21488, count: 1},
+            { id: 4, name: 'iPhone XS ', price: 10999, count: 1},
+            { id: 5, name: 'iPhone XS Max', price: 20999, count: 1}
+        ],
+        checked: false,
+        checkList: [],
     },
     computed: {
         totalPrice: function() {
             var total = 0;
-            for (var i=0; i < this.list.length; i++){
-                var item = this.list[i];
+            var goodsList = this.checkList || this.list;
+            for (var i=0; i < goodsList.length; i++){
+                var item = this.list[this.checkList[i]];
                 total += item.price * item.count;
             }
             return total.toString().replace(/\B(?=(\d{3})+$)/g, ',');
@@ -29,6 +32,17 @@ var app = new Vue({
         },
         handleRemove: function (index) {
             this.list.splice(index, 1);
-        }
+        },
+        checkAll: function() {
+            if(this.checked) {
+                this.checkList = [];
+                this.checked = false;
+            }else {
+                for (var i=0; i < this.list.length; i++){
+                    this.checkList.push(i);
+                }
+                this.checked = true;
+            }
+        },
     }
-})
+});
